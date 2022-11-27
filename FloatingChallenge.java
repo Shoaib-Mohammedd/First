d9ea1fe853b82b8d8cf57829032ff609e520f84e
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-11-27 16:32:30 +0300

    FloatingChallenge Java

---
+++ FloatingChallenge.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FloatingChallenge {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println( " Enter The Pounds To Convert Kilogram " );
+        int pound  = input.nextInt();
+        double kilogram  = 0.45359237;
+        double PoundTolKilogram = pound * kilogram;
+        System.out.println(       " " + pound + " Of Pounds Equal " + PoundTolKilogram + "  Kilogram " );
+        char myChar = 'D';
+        char myUniChar = '\u00A9';
+        System.out.println( " The Real Char Equal " + "And That Means " + myChar );
+        System.out.println( " " + myUniChar + " For SHOEIB MOHAMMED The JAVA Developer : ");
+    }
+}
+                            //200
